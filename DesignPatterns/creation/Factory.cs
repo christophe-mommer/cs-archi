745c938e703ca81aeadd520923d21f790d45ac4a
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-06-09 13:41:30 +0200

    Design Patterns with some of creation patterns

---
+++ DesignPatterns/creation/Factory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.creation
+{
+    interface IAuto
+    {
+        string Name { get; }
+        void TurnOn();
+        void TurnOff();
+    }
+    class Peugeot : IAuto
+    {
+        public string Name => "Peugeot";
+        public void TurnOff() => Console.WriteLine("Peugeot Off");
+        public void TurnOn() => Console.WriteLine("Peugeot On");
+    }
+    class Renault : IAuto
+    {
+        public string Name => "Renault";
+        public void TurnOff() => Console.WriteLine("Renault Off");
+        public void TurnOn() => Console.WriteLine("Renault On");
+    }
+
+    class Factory
+    {
+
+        public static IAuto CreateAuto(string name)
+        {
+            switch(name)
+            {
+                case "Peugeot": return new Peugeot();
+                case "Renault": return new Renault();
+                default: throw new InvalidOperationException($"This factory cannot handle {name}");
+            }
+        }
+
+    }
+}
---
+++ DesignPatterns/creation/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.creation
+{
+    class WebServiceHelper
+    {
+        private static object s_ThreadSafety;
+        private static WebServiceHelper s_Instance;
+
+        public static WebServiceHelper Instance
+        {
+            get
+            {
+                if(s_Instance == null)
+                {
+                    lock(s_ThreadSafety)
+                    {
+                        if(s_ThreadSafety == null)
+                        {
+                            s_Instance = new WebServiceHelper();
+                        }
+                    }
+                }
+                return s_Instance;
+            }
+        }
+
+        private WebServiceHelper()
+        {
+
+        }
+
+    }
+}
