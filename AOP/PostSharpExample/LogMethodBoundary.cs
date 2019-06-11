1d87737a75ac053978679f619b92ee6b02fba785
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-06-11 13:47:36 +0200

    adding a postsharp example

---
+++ AOP/PostSharpExample/LogMethodBoundary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using PostSharp.Serialization;
+
+namespace PostSharpExample
+{
+    [PSerializable]
+    public sealed class LogMethodBoundary : OnMethodBoundaryAspect
+    {
+        #region Build-Time Logic
+
+        public LogMethodBoundary()
+        {
+        }
+
+        public override bool CompileTimeValidate(MethodBase method)
+        {
+            // TODO: Check that the aspect has been applied on a proper method.
+
+            if (false)
+            {
+                Message.Write(method, SeverityType.Error, "MY001", "Cannot apply LogMethodBoundary to method '{0}'.", method);
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void CompileTimeInitialize(MethodBase method, AspectInfo aspectInfo)
+        {
+            // TODO: Initialize any instance field whose value only depends on the method to which the aspect is applied.
+        }
+
+        #endregion
+
+
+        public override void RuntimeInitialize(MethodBase method)
+        {
+            // This method is invoked once at run time.
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine("We enter the method");
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Console.WriteLine("We exit the method");
+
+        }
+
+        public override void OnSuccess(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Method was a success !");
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Method was a failure !");
+
+        }
+    }
+}
---
+++ AOP/PostSharpExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PostSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello PostSharp!");
+            MyMethod();
+            try
+            {
+                MyFailureMethod();
+            }
+            catch { }
+            Console.ReadLine();
+
+        }
+
+        [LogMethodBoundary]
+        static void MyMethod()
+        {
+            Console.WriteLine("This is just a cool method that does cool stuff");
+        }
+
+        [LogMethodBoundary]
+        static void MyFailureMethod()
+        {
+            Console.WriteLine("This method is sooo bad, it fails everytime");
+            throw new Exception();
+        }
+    }
+}
