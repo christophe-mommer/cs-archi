cd77fbb93b6fd9901e7e03c154bcc812b2f88057
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-06-11 14:18:03 +0200

    .net core version of dependency validation

---
+++ VisualStudio/VisualStudioArchitectureCore/DTOs/BookListItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VisualStudioArchitecture.Models;
+
+namespace VisualStudioArchitecture.DTOs
+{
+    public class BookListItemDTO
+    {
+        public string Title { get; private set; }
+        public string ISBN { get; set; }
+        public BookListItemDTO(Book book)
+        {
+            Title = book.Title;
+            ISBN = book.ISBN;
+        }
+    }
+}
---
+++ VisualStudio/VisualStudioArchitectureCore/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VisualStudioArchitecture.DTOs;
+
+namespace VisualStudioArchitecture.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Body { get; set; }
+        public int NbPages { get; set; }
+        public Guid Id { get; set; }
+        //public BookListItemDTO DTO { get; set; }
+    }
+}
