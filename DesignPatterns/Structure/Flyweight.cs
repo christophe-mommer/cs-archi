0553cfcd31af5c1e08f322e7944aaf68dc984f04
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-06-12 20:13:39 +0200

    flyweight draft

---
+++ DesignPatterns/Structure/Flyweight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Structure
+{
+    interface IShape
+    {
+        void Draw();
+        int X { get; }
+        int Y { get; }
+        int Height { get; }
+        int Width { get; }
+    }
+
+    class SquareShape : IShape
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public void Draw()
+        {
+
+        }
+    }
+    //class TriangleShape : IShape
+    //{
+    //    public void Draw()
+    //    {
+    //    }
+    //}
+
+    class Picture
+    {
+
+    }
+}
