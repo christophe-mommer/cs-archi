51cd9f2c801f982abc838fb532e351d33ac11fdb
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-06-12 20:13:54 +0200

    reorganize and add templates

---
+++ DesignPatterns/Behavior/State.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Behavior
+{
+    interface IPaymentState
+    {
+        void Pay(decimal amount);
+    }
+    class CreditCardPaymentState : IPaymentState
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Credit card debited ({amount}) !");
+        }
+    }
+    class PaypalPaymentState : IPaymentState
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paypal account debited ({amount}) !");
+        }
+    }
+
+    class ShoppingCart // Context
+    {
+        public IPaymentState State { get; set; }
+        public IEnumerable<string> ArticleReferences { get; set; }
+
+        public void Checkout()
+        {
+            decimal amount = 0;
+            foreach (var item in ArticleReferences)
+            {
+                amount += 10;
+            }
+            if (amount > 100)
+            {
+                State = new CreditCardPaymentState();
+            }
+            else
+            {
+                State = new PaypalPaymentState();
+            }
+            State.Pay(amount);
+        }
+    }
+}
